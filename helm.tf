f90eb6795ddd7ecb68dedec6c739c1e7cb6d2036
etwillbefine <tim.gatzemeier@gmx.de>
2020-02-16 00:12:54 +0100

    correctly reference helm repo data source

--- helm.tf
+++ helm.tf
@@ -15,7 +15,7 @@ data "helm_repository" "uswitch" {
 
 resource "helm_release" "kiam" {
   depends_on = [null_resource.apply_certs]
-  repository = helm_repository.uswitch.name
+  repository = data.helm_repository.uswitch.name
   name       = local.app_name
   chart      = "stable/kiam"
   namespace  = "kube-system"
