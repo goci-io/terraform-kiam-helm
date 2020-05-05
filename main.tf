4d45f708567365a36ddca17363c4fbfc2db63921
etwillbefine <tim.gatzemeier@gmx.de>
2020-05-05 22:20:21 +0200

    improvement(break): remove provider config use providers = { aws = aws.my-provider } to override any provider specific settings or define the provider simply in your root module

--- main.tf
+++ main.tf
@@ -2,16 +2,9 @@ terraform {
   required_version = ">= 0.12.1"
 
   required_providers {
+    aws  = "~> 2.50"
     helm = "~> 1.0"
     null = "~> 2.1"
-  }
-}
-
-provider "aws" {
-  version = "~> 2.45"
-
-  assume_role {
-    role_arn = var.aws_assume_role_arn
   }
 }
 
